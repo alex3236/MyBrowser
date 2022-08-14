3ad569ba6222e24df9ff4599d84a3139c72e0aa4
Alex3236 <alex3236@qq.com>
2022-08-14 16:39:37 +0800

    initialization

---
+++ i18n/en.js
@@ -0,0 +1,29 @@
+_ = {
+    title: {
+        project: 'MyBrowser',
+        browser: 'Browser Infomation',
+        ip: 'IP Infomation'
+    },
+    para: {
+        0: 'A simple site to get browser infomation.',
+        1: 'Note that all below is provided by your User-Agent and IP that maybe not accurate.'
+    },
+    link: {
+        define: 'WTH is this?',
+        gh: 'Check this "project" on Github'
+    },
+    item: {
+        browser: 'Browser',
+        ua: 'User Agent',
+        special_ua: ' (Special user agents are bolded)',
+        vendor: 'Vendor',
+        view: 'View Scale',
+        lang: 'Languages',
+        ip: 'IP',
+        region: 'Region'
+    },
+    note: {
+        wait_response: 'Waiting for API response...',
+        fail: 'Failed to get IP infomation.'
+    }
+}
---
+++ i18n/zh.js
@@ -0,0 +1,29 @@
+_ = {
+    title: {
+        project: 'MyBrowser',
+        browser: '浏览器信息',
+        ip: 'IP 信息'
+    },
+    para: {
+        0: '一个获取浏览器信息的垃圾页面.',
+        1: '注意，以下所有内容均由您的用户代理和 IP 提供，可能不准确.'
+    },
+    link: {
+        define: '这都啥玩意？',
+        gh: '在 Github 查看此项目'
+    },
+    item: {
+        browser: '浏览器',
+        ua: '用户代理',
+        special_ua: '（加粗的是特殊用户代理）',
+        vendor: '提供商',
+        view: '画布大小',
+        lang: '语言',
+        ip: 'IP',
+        region: '地区'
+    },
+    note: {
+        wait_response: '等待 API 响应...',
+        fail: '获取 IP 信息失败.'
+    }
+}
